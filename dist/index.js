0a74ca9ba237a28d97efeafc25217c8f5a793144
mahartariq <mahartariq499@gmail.com>
2021-02-13 00:52:19 +0500

    basic ts

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+// console.log("Hello World from Typescript");
+// let a = 11;
+// let b:number = 22;
+// let c:any = 75;
+// c = "Mahar Tariq";
+// console.log(c);
+// constant
+// let a = true;
+// let b = false;
+// b = true;
+// let c:true = true;
+// c = false;
+var user = {
+    username: "mahartariq",
+    email: "mahartariq499@gmail.com"
+};
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+// console.log("Hello World from Typescript");
+
+// let a = 11;
+// let b:number = 22;
+
+// let c:any = 75;
+// c = "Mahar Tariq";
+// console.log(c);
+
+// constant
+
+// let a = true;
+// let b = false;
+// b = true;
+
+// let c:true = true;
+// c = false;
+
+let user:{username:string,email:string} = {
+    username:"mahartariq",
+    email:"mahartariq499@gmail.com"
+};
